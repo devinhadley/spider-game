aedab55d57013842bc0f198dceafe9be2148179d
Devin Hadley <devinhadley4@gnauk.com>
2024-02-01 08:31:11 -0800

    fix: module-info

--- game/src/main/java/module-info.java
+++ game/src/main/java/module-info.java
@@ -1,9 +1,7 @@
-module com.example.spiderworld2 {
+module com.example.spiderworld {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.example.spiderworld2 to javafx.fxml;
-    exports com.example.spiderworld2;
-    exports org.studs.spidergame;
     opens org.studs.spidergame to javafx.fxml;
+    exports org.studs.spidergame;
 }
