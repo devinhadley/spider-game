699e2ed9570aaed34c8a1e7a77d09a77fc9a6b47
Luis Rodriguez <lrod011703@yahoo.com>
2024-01-31 20:14:58 -0800

    added world

--- game/src/main/java/org/studs/spidergame/Game.java
+++ game/src/main/java/org/studs/spidergame/Game.java
@@ -3,22 +3,25 @@ package org.studs.spidergame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Game extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("spiderworld.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1250, 800);
-        stage.setTitle("Spider World");
+        Scene scene = new Scene(fxmlLoader.load(), 1250, 800, Color.BLUE);
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
+
+
 }
