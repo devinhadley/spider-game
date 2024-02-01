09d1ebdf9c5a257b62678e6b0799206bd7a0c239
Devin Hadley <devinhadley4@gnauk.com>
2024-01-31 19:06:49 -0800

    fix: migrated to fxml

--- game/src/main/java/org/studs/spidergame/Game.java
+++ game/src/main/java/org/studs/spidergame/Game.java
@@ -3,16 +3,18 @@ package org.studs.spidergame;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Game extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500, Color.BLUE);
         stage.setScene(scene);
         stage.show();
     }
@@ -20,4 +22,6 @@ public class Game extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
